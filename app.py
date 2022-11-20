900a5056a63ae130c057e1cb06d6996b1041bef5
alaska16 <ali.khelfaoui@icloud.com>
2022-11-20 22:18:29 +0100

    debug server window

--- app.py
+++ app.py
@@ -1,47 +1,36 @@
-# MyHomeApp v0.2a (2022) - Ali
+# MyHomeApp v0.1
 # MyHome is a home management app, specifically tailored for my home.
 # ===================================================================
 # This first version is a prototype to see what works well and what does not.
 # At this point, I am still unsure about what I want to do with this project; I don't know if I want to make it public, if it has to be "other-users friendly", etc.
 # I am still learning Python as I am developing this app so bugs and unoptimized code are expected. 
 # I want to go with a Windows Aero-like design (Windows 7 wallpaper, WLM 2012-era, Windows Vista weather widget, glass, thin Segoe-like fonts, ...)
-# === Features per version : ===
-# v0.1p : design prototype
-# v0.2a : networking, client/server architecture
-# v0.3a : hardware integration
-# v0.4a : wallpaper and weather(fixed picture, dynamic wallpaper according to the current weather)
-# v0.5a : dark mode at sunset
-# v0.6a : parameters charts (temperatures, humidity, ...)
-# v0.7a : écowatt
-# v0.8a : settings screen
+# ❤️
 
-# Libraries imports
-from tkinter import N
+# -- Imports --
+import logging
 import pygame
-import sockets
+import socket
 import threading
 import datetime
 from subprocess import call
-pygame.init()
-print("Libraries are imported and initialized.") # Soon to be replaced with (import logging)
 
-# Variables
+# -- Variables --
+connected = False
 width = 640
 height = 480
 white = (255, 255, 255)
 black = (0, 0, 0)
 grey = (128, 128, 128)
-t_livingroom = 22
-h_livingroom = 78
-t_room1 = 23
-h_room1 = 79
-t_room2 = 24
-h_room2 = 80
-t_kitchen = 25
-h_kitchen = 81
-time = "notset"
+t_livingroom = 0
+t_room1 = 0
+t_room2 = 0
+h_livingroom = 0
+h_room1 = 0
+h_room2 = 0
+time = "00:00 (notset)"
 
-# Fonctions
+# -- Fonctions --
 def server():
     call(["python3", "server.py"])
 
@@ -56,31 +45,25 @@ def get_date():
     return date
 
 def interface():
-    screen.blit(default_wallpaper, (0, 0))
-    screen.blit(livingroom_t_str, (16, 16))
-    screen.blit(temp_livingroom, (16, 20)) #
-    screen.blit(humidity_livingroom, (172, 20))
-    screen.blit(room1_t_str, (16, 144))
-    screen.blit(temp_room1, (16, 152)) #
-    screen.blit(humidity_room1, (172, 152))
-    screen.blit(room2_t_str, (16, 276))
-    screen.blit(temp_room2, (16, 284)) #
-    screen.blit(humidity_room2, (172, 284))
-    screen.blit(time_str, (360, 16))
-    screen.blit(date_str, (360, 80))
-    # screen.blit(placeholder64png, (128, 40)) # Temperature icon
-    # screen.blit(placeholder64png, (128, 172)) # Temperature icon
-    # screen.blit(placeholder64png, (128, 304)) # Temperature icon
-    # screen.blit(placeholder64png, (192, 40)) # Humidity icon
-    # screen.blit(placeholder64png, (192, 172)) # Humidity icon
-    # screen.blit(placeholder64png, (192, 304)) # Humidity icon
-    # screen.blit(placeholder64png, (16, 16))
-    # screen.blit(placeholder128png, (480, 16))
-    # pygame.draw.line(screen, black, (0, 240), (640, 240))
+    mainwindow.blit(default_wallpaper, (0, 0))
+    mainwindow.blit(livingroom_t_str, (16, 16))
+    mainwindow.blit(temp_livingroom, (16, 20)) #
+    mainwindow.blit(humidity_livingroom, (172, 20))
+    mainwindow.blit(room1_t_str, (16, 144))
+    mainwindow.blit(temp_room1, (16, 152)) #
+    mainwindow.blit(humidity_room1, (172, 152))
+    mainwindow.blit(room2_t_str, (16, 276))
+    mainwindow.blit(temp_room2, (16, 284)) #
+    mainwindow.blit(humidity_room2, (172, 284))
+    mainwindow.blit(time_str, (360, 16))
+    mainwindow.blit(date_str, (360, 80))
 
-# Screen and loading assets
-screen = pygame.display.set_mode((width, height))
+# -- Screen --
+pygame.init()
+mainwindow = pygame.display.set_mode((width, height))
 pygame.display.set_caption("MyHome App v0.2a")
+
+# -- Assets --
 default_font16 = pygame.font.Font(None, 16)
 default_font32 = pygame.font.Font(None, 32)
 default_font64 = pygame.font.Font(None, 64)
@@ -101,15 +84,13 @@ temp_room1 = haskoy_bold128.render(str(t_room1), True, black)
 room2_t_str = haskoy_extralight32.render("Parents' room", True, black)
 humidity_room2 = haskoy_medium128.render(str(h_room2), True, black)
 temp_room2 = haskoy_bold128.render(str(t_room2), True, black)
-# time_str = font64.render((time), True, black)
-# kitchen_str = font32.render("Kitchen", True, black)
-# temp_kitchen = font128.render(str(t_kitchen), True, black)
 
-# Launch server
+# -- Launch server --
 serverThread = threading.Thread(target=server)
 serverThread.start()
 
-# Main loop
+
+# -- Main loop --
 while True:
     time_str = haskoy_medium64.render((get_time()), True, black)
     date_str = haskoy_extralight32.render((get_date()), True, black)
--- server.py
+++ server.py
@@ -1,4 +1,33 @@
-# MyHome Server v0.2a (2022) - Ali
-import sockets
-import tkinter
-print("Librairies du serveur chargées et initalisées.")
+# MyHome Server v0.1
+import logging
+import socket
+from tkinter import *
+
+# To figure out later: is it better to send all the data in one single "cycle" or should it close the socket every time?
+
+# -- Interface --
+serverdebugwindow = Tk()
+t_livingroom = IntVar()
+t_room1 = IntVar()
+t_room2 = IntVar()
+# - Living room -
+slider_livingroom = Scale(serverdebugwindow, label = "Living room", variable= t_livingroom)
+slider_livingroom.pack(anchor = CENTER)
+# - Room 1 -
+slider_room1 = Scale(serverdebugwindow, label = "Room1", variable= t_room1)
+slider_room1.pack(anchor = CENTER)
+# - Room 2 -
+slider_room2 = Scale(serverdebugwindow, label = "Room2", variable= t_room2)
+slider_room2.pack(anchor = CENTER)
+# - Loop -
+serverdebugwindow.mainloop()
+
+# -- Variables --
+SERVER_HOST = "0.0.0.0"
+logging.info("Server is configured to listen to all IP adresses of this system.")
+SERVER_PORT = 5555
+BUFFER_SIZE = 4096
+logging.info("Buffer size : 4096")
+SEPARATOR = "<SEPARATOR>"
+
+print(t_livingroom)
